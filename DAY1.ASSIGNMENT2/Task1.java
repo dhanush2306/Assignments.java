8851dff7802b6ea575c14cd578538b38b41675c5
dhanush2306 <130884380+dhanush2306@users.noreply.github.com>
2025-12-31 10:16:28 +0530

    push

--- DAY1.ASSIGNMENT2/Task1.java
+++ DAY1.ASSIGNMENT2/Task1.java
@@ -1,7 +1,19 @@
 
+import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int correctPin =2306;
+        int enteredPin =0;
+       while (enteredPin !=correctPin) {
+        System.out.println("eneter your pin");
+        enteredPin = sc.nextInt();
+        if (enteredPin !=correctPin) {
+            System.out.println("wrong pin !! try again");
+        }
+        }
+        System.out.println("pin correct!! Access granted");
+        sc.close();
     }
 }
--- Task1.java
+++ Task1.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Task1{
     public static void main(String[] args) {
+    Scanner laks= new Scanner(System.in);   
+    System.out.println("Enter your mark");  
+    int obj  =laks.nextInt();   
         int marks = 75;
         if (marks>=35) {
             System.out.println("pass");
